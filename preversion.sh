f139ca5e2db5db6b0c590dcb584df924762b4c7b
Mirek Rusin <mirek@me.com>
2021-11-07 20:31:06 +0100

    Fixing cjs/mjs.

--- preversion.sh
+++ preversion.sh
@@ -1,11 +1,15 @@
 
 # cjs
 rm -Rf cjs
-npx tsc -m commonjs -d --outDir cjs
+npx tsc -m commonjs -d --sourceMap --outDir cjs
 echo '{"type":"commonjs"}' > cjs/package.json
 
 # mjs
-npx tsc
+rm -Rf mjs
+npx tsc -d --sourceMap --outDir mjs
+
+# mjs test
+npx tsc -m commonjs -p test/tsconfig.json
 
 # test
 npm test
