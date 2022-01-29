4ce1d0216a2dcc14524b51727bdcc5c222732273
Mirek Rusin <mirek@me.com>
2022-01-29 09:18:40 +0100

    Adding arrays.

---
+++ src/arrays.test.ts
@@ -0,0 +1,30 @@
+import * as Cmp from './index.js'
+
+test('same', () => {
+  expect(Cmp.arrays(Cmp.numbers)(
+    [ 3, 5, 4 ],
+    [ 3, 5, 4 ]
+  )).toBe(Cmp.equal)
+})
+
+test('asc', () => {
+  expect(Cmp.arrays(Cmp.numbers)(
+    [ 3, 5, 4 ],
+    [ 3, 6, 4 ]
+  )).toBe(Cmp.asc)
+  expect(Cmp.arrays(Cmp.numbers)(
+    [ 3, 5, 4 ],
+    [ 3, 5, 4, 1 ]
+  )).toBe(Cmp.asc)
+})
+
+test('dsc', () => {
+  expect(Cmp.arrays(Cmp.numbers)(
+    [ 3, 6, 4 ],
+    [ 3, 5, 4 ]
+  )).toBe(Cmp.dsc)
+  expect(Cmp.arrays(Cmp.numbers)(
+    [ 3, 5, 4, 1 ],
+    [ 3, 5, 4 ]
+  )).toBe(Cmp.dsc)
+})
---
+++ src/arrays.ts
@@ -0,0 +1,25 @@
+import { asc, equal, dsc, type Cmp } from './prelude.js'
+import numbers from './numbers.js'
+
+/**
+ * Compares arrays element-wise up to common length.
+ * If all are equal then returns comparision result on array lengths.
+ */
+export const arrays =
+  <T>(cmp: Cmp<T>) =>
+    (as: T[], bs: T[]) => {
+      const n = Math.min(as.length, bs.length)
+      for (let i = 0; i < n; i++) {
+        switch (cmp(as[i], bs[i])) {
+          case asc:
+            return asc
+          case equal:
+            break
+          case dsc:
+            return dsc
+        }
+      }
+      return numbers(as.length, bs.length)
+    }
+
+export default arrays
