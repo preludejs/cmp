cab9834ca64850536dc65c1d64168ebe5fa848ea
Mirek Rusin <mirek@me.com>
2022-01-29 09:19:03 +0100

    Updating export.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
+import arrays from './arrays.js'
 import chain from './chain.js'
 import epsilon from './epsilon.js'
 import map from './map.js'
+import max from './max.js'
+import min from './min.js'
 import numbers from './numbers.js'
 import partial from './partial.js'
 import partialB from './partial-b.js'
@@ -11,9 +14,12 @@ import strings from './strings.js'
 export * from './prelude.js'
 
 export {
+  arrays,
   chain,
   epsilon,
   map,
+  max,
+  min,
   numbers,
   partial,
   partialB,
