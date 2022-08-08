6d603ba231714eecf7db361344b784578ab7bea4
Mirek Rusin <mirek@me.com>
2022-08-08 08:43:35 +0200

    feat(lhs,rhs): support distinct lhs and rhs types

--- src/prelude.ts
+++ src/prelude.ts
@@ -1,14 +1,18 @@
 /** Comparision result type. */
-export type R = -1 | 0 | 1
+export type R =
+  | -1
+  | 0
+  | 1
 
 /** Comparision function type. */
-export type Cmp<T> = (a: T, b: T) => R
+export type Cmp<Lhs, Rhs = Lhs> =
+  (lhs: Lhs, rhs: Rhs) => R
 
 /** Curried comparision function type. */
 export type Cmp1<T> = (a: T) => R
 
 /** Comparision function type. */
-export type t<T> = Cmp<T>
+export type t<Lhs, Rhs = Lhs> = Cmp<Lhs, Rhs>
 
 export type Tail<T> =
   T extends [_: unknown, ...tail: infer R] ?
