5a84bb55f522b494cfe364160ab3e879e7b938f8
Mirek Rusin <mirek@me.com>
2024-06-15 19:23:10 +0200

    feat: maybe type

--- src/prelude.ts
+++ src/prelude.ts
@@ -4,10 +4,20 @@ export type R =
   | 0
   | 1
 
+/** Comparision result that may not be defined. */
+export type MaybeR =
+  | undefined
+  | R
+
 /** Comparision function type. */
 export type Cmp<Lhs, Rhs = Lhs> =
   (lhs: Lhs, rhs: Rhs) =>
     R
+
+/** Comparision function that may not return comparision result. */
+export type MaybeCmp<Lhs, Rhs = Lhs> =
+  (lhs: Lhs, rhs: Rhs) =>
+    MaybeR
 
 /** Curried comparision function type. */
 export type Cmp1<T> =
